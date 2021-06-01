e0062811bd4096bf8a9af3b428654aa6a63c78c8
Aminata Kabba <aminata-kabba@hotmail.com>
2021-06-01 15:16:55 +0100

    babel & parcel packages added

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log('Hi')
+
+// const element = React.createElement('div', null, 'React element!')
+
+const element = <div>React with JSX</div>
+
+ReactDOM.render(element, document.getElementById('root'))
